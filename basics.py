bc2df51b87313a80b01065030b69bb7ea29c01c4
bhavyasri0331 <gajapurambhavya.in>
2026-02-20 10:12:52 +0530

    Initial commit

---
+++ basics.py
@@ -0,0 +1,23 @@
+print("starting my day1")
+print(5)
+print(8+12)
+print(8*12)
+print(8/12)
+print(8//12)
+#comments used to understand other user quickly (single line)
+'''this is used for multiple line comment
+consistency is the key
+\n - used fo escape sequence of line in characters'''
+print("hey \nhi everyone")
+#data types and variables
+a=10
+b='bhavya'
+c=True
+print(a)
+print(type(a))
+print(type(b))
+print(type(c))
+list=[1,2.2,'python']
+print(list)
+tuple={'python','learner'}
+print(tuple)
